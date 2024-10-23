0a9ca814765bc970597af0de0f199484ceca5f82
lexaeliseev <lexaeliseev@gmail.com>
2024-10-23 10:50:42 +0300

    add driver_options = eager

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,19 @@
 import pytest
 from selene import browser
+from selenium import webdriver
 
 
 @pytest.fixture(autouse=True, scope="session")
 def browser_settings():
+
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options
+
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.base_url = "https://demoqa.com"
+
     yield
+
     browser.quit()
